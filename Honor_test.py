945df15b458d63362f8b1e415b39c7211ab3d0fe
unknown <fforse@example.com>
2023-11-24 15:29:07 +0100

    Commented out neo4j

--- Honor_test.py
+++ Honor_test.py
@@ -30,9 +30,11 @@ honorModel = model.Model("honormodel", langSpec, pythClasses)
 honorModel.load_from_file("honor_test.json")
 
 # Save in a temp json file
+
 # honorModel.save_to_file("tempModel.json")
 
 # Generate attackgraps from model
+
 attkgraph = attackgraph.AttackGraph()
 attkgraph.generate_graph(langSpec, honorModel)
 apriori.calculate_viability_and_necessity(attkgraph)
@@ -40,9 +42,13 @@ attkgraph.attach_attackers(honorModel)
 # attkgraph.save_to_file("attackgraph_file.json")
 
 # Create neo4j model instance
-neo4j.ingest_model(honorModel, "neo4j://localhost:7687", "Fredrik",
-"fred4551", "ingestmodel", delete=True)
+
+#neo4j.ingest_model(honorModel, "neo4j://localhost:7687", "Fredrik",
+#"fred4551", "ingestmodel", delete=True)
 
 # Create neo4j attackgraph instance
-neo4j.ingest_attack_graph(attkgraph, "neo4j://localhost:7687", "Fredrik",
-"fred4551", "attackgraphs", delete=True)
+
+#neo4j.ingest_attack_graph(attkgraph, "neo4j://localhost:7687", "Fredrik",
+#"fred4551", "attackgraphs", delete=True)
+
+
