83158586266cf494b25e68cb0f2b602af48c5c9e
unknown <fforse@example.com>
2023-11-27 15:47:46 +0100

    Deleted asset with its associations, Flex agent DB and Flex Asset Mgmt

--- Honor_test.py
+++ Honor_test.py
@@ -12,41 +12,52 @@ from maltoolbox.attackgraph import attackgraph
 from maltoolbox.attackgraph.analyzers import apriori
 from maltoolbox.ingestors import neo4j
 
-# Load the language specification, coreLang mar file
+""" Load the language specification, coreLang mar file """
 langSpec = specification.load_language_specification_from_mar("mal_corelang_test.mar")
 
-# Prints attacksteps for the assets RoutingFirewall
+""" Prints attacksteps for the assets RoutingFirewall """
 # attks = specification.get_attacks_for_class(langSpec,"RoutingFirewall")
 # print(attks)
 
-# Generate python classes from specification
+""" Generate python classes from specification """
 pythClasses = classes_factory.LanguageClassesFactory(langSpec)
 pythClasses.create_classes()
 
-#Create a model object
+"""Create a model object """
 honorModel = model.Model("honormodel", langSpec, pythClasses)
 
-# Load model from json format
+""" Load model from json format """
 honorModel.load_from_file("honor_test.json")
 
-# Save in a temp json file
+""" Find assets by id Test  """
+findAsset = honorModel.get_asset_by_id(5439473563977401412)
 
-# honorModel.save_to_file("tempModel.json")
+""" Remove the specified assets associations (all associations are bi-directional so only need to remove once between 2 nodes)"""
+honorModel.delete_associations_by_asset_id(5439473563977401412)
 
-# Generate attackgraps from model
 
-attkgraph = attackgraph.AttackGraph()
-attkgraph.generate_graph(langSpec, honorModel)
-apriori.calculate_viability_and_necessity(attkgraph)
-attkgraph.attach_attackers(honorModel)
+""" Remove a specified asset by its id"""
+honorModel.delete_asset_by_id(5439473563977401412)
+honorModel.delete_asset_by_id(3968070349114787769)
+
+
+""" Save in a temp json file """
+honorModel.save_to_file("tempModel.json")
+
+""" Generate attackgraps from model """
+
+#attkgraph = attackgraph.AttackGraph()
+#attkgraph.generate_graph(langSpec, honorModel)
+#apriori.calculate_viability_and_necessity(attkgraph)
+#attkgraph.attach_attackers(honorModel)
 # attkgraph.save_to_file("attackgraph_file.json")
 
-# Create neo4j model instance
+""" Create neo4j model instance """
 
 #neo4j.ingest_model(honorModel, "neo4j://localhost:7687", "Fredrik",
 #"fred4551", "ingestmodel", delete=True)
 
-# Create neo4j attackgraph instance
+""" Create neo4j attackgraph instance """
 
 #neo4j.ingest_attack_graph(attkgraph, "neo4j://localhost:7687", "Fredrik",
 #"fred4551", "attackgraphs", delete=True)
